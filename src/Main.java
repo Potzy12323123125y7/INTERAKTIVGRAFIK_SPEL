68b8f889e3a824f370ae0f2bda6a3e8220961ca5
Potzy12323123125y7 <alexander.naslund2@elev.ga.ntig.se>
2020-05-14 11:20:25 +0200

    2020-05-14 Försökt göra poäng system

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Graphics graphics = new Graphics(400,300,2);
         graphics.start();
+        Score.start();
     }
 }
---
+++ src/Score.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Score extends JFrame {
+    private final static int width = 200, height = 200;
+    private Score panel;
+
+    public Score() {
+        setSize(width,height);
+        new JFrame("score").setVisible(true);
+
+    }
+
+    public static void start() {
+
+    }
+}
